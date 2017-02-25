39d075e62904c49590b3fd95b4eb3281a37e5574
cacochen <cacochenwei@gmail.com>
2017-02-25 02:01:02 -0500

    Export and require function trial

---
+++ setTimeout.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Domain on 2/25/2017.
+ */
+console.log('1. Start');
+
+var timer1 = setTimeout(function() {
+   console.log('2. In the timeout');
+},500);
+
+console.log('3.end');
